ba55308d3e79e9c811faf787e97e03f19885b77b
Q池袋 <qikebukuro@Mac20151105-18-no-MacBook-Air.local>
2019-03-30 13:22:48 +0900

    first commit

---
+++ Assets/Muzzle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+
+    //public GemeObject bullet;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+       // if (Input.GetMouseButtonDown(0))
+        {
+            //Shot();
+        }
+    }
+}
